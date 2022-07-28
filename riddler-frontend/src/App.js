7b116af40dd4af79acd238068988f8f3fbaece8e
Arjun Khanna <arjunkhanna0108@gmail.com>
2022-07-28 22:18:03 +0530

    basic layout  added

--- riddler-frontend/src/App.js
+++ riddler-frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import door from "./door.jpg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Riddler 2022</h1>
+			<div className="doors">
+				<img src={door} alt="XD" className="door-1" />
+				<img src={door} alt="XD" className="door-2" />
+				<img src={door} alt="XD" className="door-3" />
+				<img src={door} alt="XD" className="door-4" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
